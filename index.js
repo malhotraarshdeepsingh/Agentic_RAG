01d5fb0edac28cda0d1d43dfff462d25085c772d
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-07-09 17:49:28 +0530

    implement OpenAI integration for message handling

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import { Server as socket } from "socket.io";
 import http from "http";
 import path from "path";
+import OpenAI from "openai";
 import dotenv from "dotenv";
-
 
 // Load environment variables from .env file
 dotenv.config();
@@ -30,6 +30,11 @@ server.listen(3000, function () {
   console.log("Server listening on port 3000");
 });
 
+// Making Open AI instance
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 // Socket Code
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -37,9 +42,18 @@ io.on("connection", (socket) => {
   // Handle incoming messages
   socket.on("message", async (data) => {
     console.log("Message received:", data);
-    
-    const hello = `Bot: You said "${data}""`;
-    
+
+    // const hello = `Bot: You said "${data}""`;
+    const prompt = `${data}`;
+
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      max_tokens: 400,
+      prompt,
+    });
+
+    // console.log("Response from OpenAI:", response);
+    const reply = response.choices[0].text.trim();
     socket.emit("message", reply);
   });
 
